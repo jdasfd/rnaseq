cafc827953d60042359581ebdc56bf3e0a767807
Yuqian Jiang <55595834+jdasfd@users.noreply.github.com>
2024-06-05 16:27:17 +0800

    Update kegg_extract.r
    update some notes

--- scripts/kegg_extract.r
+++ scripts/kegg_extract.r
@@ -1,5 +1,21 @@
 #!/usr/bin/env Rscript
 
+# Env dependence
+# R (4.2.3)
+# Bioconductor (3.18)
+
+# Usage
+# Rscript kegg_extract.r <hsa_number>
+
+# If there is anything wrong, please following the steps here:
+
+# if (!require("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager")
+# BiocManager::install(version = "3.19")
+# 
+# packages <- c("KEGGREST", "EnrichmentBrowser", "clusterProfiler")
+# BiocManager::install(setdiff(packages, rownames(installed.packages())))
+
 args <- commandArgs(T)
 
 if (!grepl("hsa", args[1])) {
@@ -11,7 +27,7 @@ suppressMessages(library(EnrichmentBrowser))
 suppressMessages(library(clusterProfiler))
 
 # init
-out <- paste0(args[1], ".lst")
+out <- paste0(args[1], ".txt")
 
 # download the pathway
 hsapathway <- downloadPathways("hsa")
@@ -23,9 +39,12 @@ hsa <- getGenesets(org = "hsa", db = "kegg", gene.id.type = "SYMBOL", cache = TR
 writeGMT(hsa, gmt.file = "kegg_hsa.tmp.gmt")
 kegg <- clusterProfiler::read.gmt("kegg_hsa.tmp.gmt")
 
+# extract the subset according to the hsa accession
+# if you want to directly search this, please replace args[1] to "hsa+numer"
 keggsub <- subset(kegg, grepl(args[1], kegg$term))
 genelist <- keggsub$gene
 
 # write out result and empty the tmp file
+# if you do not want this step, skip write table or just use your own way to export
 write.table(genelist, file = out, quote = FALSE, row.names = FALSE, col.names = FALSE)
 file.remove("kegg_hsa.tmp.gmt")
